abcb846f387100f407bb4e654a609d5cc432beb5
hamid haghverdi <hamidhaghverdi2002@gmail.com>
2024-05-06 06:02:29 -0700

    EntityFrameworkCore Added

---
+++ StudentEnrollmentApp/StudentEnrollment.Data/Entities/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentEnrollment.Data.Entities
+{
+    public class Student : BaseEntity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateofBirth { get; set; }
+        public string IdNumber { get; set; }
+        public string Picture { get; set; }
+    }
+}
